02c5597a66b3f97f2f41f27b676d745aac149a92
rushikesh <rushikeshteli050@gmail.com>
2023-04-05 13:39:43 +0530

    first commit

---
+++ db/users.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+name:{type:String ,required:true},
+email:{type:String,require:true},
+password:{type:String,require:true}
+})
+
+module.exports =mongoose.model('users',userSchema);
